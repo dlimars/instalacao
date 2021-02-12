eaa0e4ab5ffb63c0dde916a7404ff91ebd4db49e
Daniel Lima <daniel@docode.com.br>
2021-02-11 22:26:43 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ IP="$(hostname -i)";
 VERSAO_SO="$(cat /etc/redhat-release)";
 read -p "Informe o domínio público da API ex: ( api.dominio.com.br ) : " API_URL;
 read -p "Informe o domínio público da CDN ex: ( cdn.dominio.com.br ) : " CDN_URL;
+
+echo "TESTE";
