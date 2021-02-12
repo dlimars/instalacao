0db7b2ed02f228cfa9343377ae94e76ddddeaffe
Daniel Lima <daniel@docode.com.br>
2021-02-11 23:13:16 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,23 +16,25 @@ read -p "Informe o domínio público da CDN ex: ( cdn.dominio.com.br ) : " CDN_U
 
 ### INSTALAR PACOTES NECESSÁRIOS
 if isinstalled "git"; then
-    echo "Dependências já instaladas ..."
+    echo "Dependências já foram instaladas anteriormente ..."
 else
     echo "Instalando dependências e utilitários"
     sudo yum -y install git wget
     curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.sh | bash
     nvm install 10.14.1
     npm install pm2@latest -g
+fi;
 
-    ### CONFIGURAR VERSÃO PADRÃO DO NODE
-    echo "Configurando versão default do node"
-    nvm alias default v10.14.1
-    nvm use v10.14.1
+### CONFIGURAR VERSÃO PADRÃO DO NODE
+if [[ $(which npm) ]]; then
+    echo "Configurando versão default do node";
+    nvm alias default v10.14.1;
+    nvm use v10.14.1;
 fi;
 
 ### INSTALAÇÃO DO DOCKER
 if [[ $(which docker) && $(docker --version) ]]; then
-    echo "Docker já instalado ..."
+    echo "Docker já foi instalado anteriormente ..."
 else
     echo "Instalando docker ..."
     wget -qO- https://get.docker.com/ | sh
@@ -43,7 +45,7 @@ fi;
 
 ### INSTALAÇÃO DO DOCKER-COMPOSE
 if [[ $(which docker-compose) ]]; then
-    echo "Docker-Compose já instalado ..."
+    echo "Docker-Compose já foi instalado anteriormente ..."
 else
     sudo curl -L --fail https://github.com/docker/compose/releases/download/1.25.0/run.sh -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
@@ -51,7 +53,7 @@ fi;
 
 ### INSTALAÇÃO DO POSTGRES
 if isinstalled "postgresql11"; then
-    echo "Postgres já foi instalado anteriormente"
+    echo "Postgres já foi instalado anteriormente ..."
 else
     echo "Instalando postgres ..."
     sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
@@ -81,14 +83,14 @@ if ! [[ -d "/opt/bdti" ]]; then
 fi;
 
 if ! [[ -d "/opt/bdti/hydra" ]]; then
-    cd /opt/bdti && git clone git@gitlab.com:bdti/hydra.git
+    cd /opt/bdti && git clone --depth 1 -b master git@gitlab.com:bdti/hydra.git
 fi;
 
 if ! [[ -d "/opt/bdti/b2b" ]]; then
     while true; do
         read -p "Clonar plataforma de B2B? [y/n]?" yn
         case $yn in
-            [Yy]* ) cd /opt/bdti && git clone git@gitlab.com:bdti/bd-2b-client.git b2b; break;;
+            [Yy]* ) cd /opt/bdti && git clone --depth 1 -b master git@gitlab.com:bdti/bd-2b-client.git b2b; break;;
             [Nn]* ) break;;
             * ) echo "Apenas Y ou N";;
         esac
@@ -99,7 +101,7 @@ if ! [[ -d "/opt/bdti/call" ]]; then
     while true; do
         read -p "Clonar plataforma de CallCenter? [y/n]?" yn
         case $yn in
-            [Yy]* ) cd /opt/bdti && git clone git@gitlab.com:bdti/bd-call-client.git call; break;;
+            [Yy]* ) cd /opt/bdti && git clone --depth 1 -b master git@gitlab.com:bdti/bd-call-client.git call; break;;
             [Nn]* ) break;;
             * ) echo "Apenas Y ou N";;
         esac
@@ -107,11 +109,11 @@ if ! [[ -d "/opt/bdti/call" ]]; then
 fi;
 
 if ! [[ -d "/opt/bdti/adm" ]]; then
-    cd /opt/bdti && git clone git@gitlab.com:bdti/bd-call-client.git adm;
+    cd /opt/bdti && git clone --depth 1 -b master git@gitlab.com:bdti/bd-call-client.git adm;
 fi;
 
 if ! [[ -d "/opt/bdti/cdn" ]]; then
-    cd /opt/bdti && git clone git@gitlab.com:bdti/bd-cdn.git cdn;
+    cd /opt/bdti && git clone --depth 1 -b master git@gitlab.com:bdti/bd-cdn.git cdn;
 fi;
 
 ## CONFIGURAÇÃO DO CALLCENTER ( SE O DIRETÓRIO EXISTIR )
@@ -181,6 +183,8 @@ if ! [[ -f "/opt/bdti/hydra/.env" ]]; then
     cp env-example .env
     sed -i "s/localhost/$IP/" .env
 fi;
+
+echo "Adicionando entradas no iptables e firewall";
 
 ### LIBERAÇÃO DE PORTAS NO IPTABLES E FIREWALL
 sudo iptables -A INPUT -p tcp --dport 5432 -j ACCEPT
