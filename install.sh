c3f0dc85db3f921cd6dff0964647babf13ad8eba
Daniel Lima <daniel@docode.com.br>
2021-02-11 21:45:25 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Instalando postgres ..."
+sudo yum install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+sudo yum install postgresql11 postgresql11-server postgresql11-contrib
+/usr/pgsql-11/bin/postgresql-11-setup initdb
+systemctl enable postgresql-11
+systemctl start postgresql-11
+
