5e28aee1cc1fcc432948d7cebd4563934d5d2d34
Daniel Lima <daniel@docode.com.br>
2021-02-11 22:52:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-### OBTÉM O IP LOCAL DO SERVIDOR E URL DE API
-IP="$(hostname -i)";
-VERSAO_SO="$(cat /etc/redhat-release)";
-read -p "Informe o domínio público da API ex: ( api.dominio.com.br ) : " API_URL;
-read -p "Informe o domínio público da CDN ex: ( cdn.dominio.com.br ) : " CDN_URL;
-
-echo "TESTE";
+echo "Clonar plataforma de B2B?"
+select yn in "y" "n"; do
+    case ${yn} in
+        Yes ) cd /opt/bdti && git clone git@gitlab.com:bdti/bd-2b-client.git b2b; break;;
+    esac
+done
