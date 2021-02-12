896d1dbfbeb76bd6ab52de6147c6136830d4f464
Daniel Lima <daniel@docode.com.br>
2021-02-11 22:53:11 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 echo "Clonar plataforma de B2B?"
 select yn in "y" "n"; do
-    case ${yn} in
+    case $yn in
         Yes ) cd /opt/bdti && git clone git@gitlab.com:bdti/bd-2b-client.git b2b; break;;
     esac
 done
